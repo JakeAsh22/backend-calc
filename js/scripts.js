5599b46547852bddecec05fef01aa8e380a34db5
Jake Ash, Jamila Aliyeva and Trevor Mackin <jamila@>
2019-10-21 11:41:08 -0700

    will add the javascript functions for calc backend

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = add(number1, number2);
+// alert(result);
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = subtract(number1, number2);
+// alert(result);
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = multiply(number1, number2);
+// alert(result);
+//
+// var divide = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var result = divide(number1, number2);
+// alert(result);
+//
+// add (5,3);
+// subtract(7,6);
+// multiply(6,6);
+// divide(6,2);
+//
+// var myHeight = prompt("Enter height");
+//
+// var myWeight = prompt("enter weight");
+//
+// var BMI = function(height,weight) {
+//     alert (parseFloat((weight/height)*703));
+// }
+//
+// BMI (myHeight, myWeight);
+//
+//
+//
+// var fahrenheit = prompt("Enter temp in fahrenheit");
+//
+// var convertFtoC = function(fahrenheit) {
+//     return parseFloat(((fahrenheit - 32) * 5) / 9);
+// }
+//
+// alert(convertFtoC(fahrenheit));
+
+
+var gallonsToLiters = function (gallons) {
+  return parseFloat(gallons / 0.26417);
+}
+alert(gallonsToLiters());
